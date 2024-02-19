1660c8288315373d5a7a34811ebd9d72517a8bc8
spykelionel <social.lionel@gmail.com>
2024-02-19 13:45:47 +0100

    add computer class and Player class as a type of Player

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
+// forward declaration
+class Player;
+class HumanPlayer;
+class System;
+
+class Player {
+    private:
+        string name;
+    public:
+    Player(string pName): name(pName) {}
+    virtual void play() {
+        cout <<"Player "<<name<<" is playing."<<endl;
+    }
+};
+
+class HumanPlayer: public Player {
+    private: string name;
+    public:
+        HumanPlayer(string name):name(name), Player(name){}
+
+};
+
+class System: public Player {
+    private: string name;
+    public:
+        System(string name="Computer"): name(name), Player(name){}
+};
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    System sys;
+    sys.play();
     return 0;
 }
