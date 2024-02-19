8a69216ba97b7634bb14bcbd2443a9c77e2759cd
spykelionel <social.lionel@gmail.com>
2024-02-19 23:53:44 +0100

    create a vector of players

--- main.cpp
+++ main.cpp
@@ -18,11 +18,17 @@ struct CurrentPlayer
     string name;
 };
 
+struct Point
+{
+    int turn;
+    int points;
+    string name;
+};
 class Player
 {
 private:
     string name;
-    vector<string> players;
+    vector<Point> players;
     int points;
 
 public:
@@ -51,22 +57,21 @@ public:
         }
     }
 
-    void addPlayer(string name)
-    {
-        players.push_back(name);
-    }
-
-    vector<string> getPlayers()
-    {
-        return players;
-    }
-
     int getPoints() const { return points; }
     void addPoints(int point)
     {
         points += point;
     }
     string getName() { return name; }
+    void setPoints(string name, int turn, int points)
+    {
+        Point p;
+        p.name = name;
+        p.turn = turn;
+        p.points = points;
+        players.push_back(p);
+    }
+    vector<Point> getPlayers() const { return players; }
 };
 
 class Human : public Player
@@ -76,6 +81,8 @@ private:
 
 public:
     Human(string name) : name(name), Player(name) {}
+    Human() {}
+    string getName() { return name; }
 };
 
 class System : public Player
@@ -91,27 +98,26 @@ class Die
 {
 private:
     int faceValue;
-    static mt19937 rng;
-    static uniform_int_distribution<int> distribution;
 
 public:
     Die()
     {
-        rng.seed(time(0));
+        faceValue = 13;
     }
     void roll()
     {
-        faceValue = getRandomNumber();
+        try
+        {
+            faceValue = rand() % 6 + 1;
+        }
+        catch (const exception &e)
+        {
+            cerr << e.what() << '\n';
+        }
     }
     int getFaceValue()
     {
-        return faceValue;
-    }
-    int getRandomNumber()
-    {
-        mt19937 rng(time(0));
-        uniform_int_distribution<int> distribution(1, 6);
-        return distribution(rng);
+        return this->faceValue;
     }
 };
 
@@ -119,24 +125,84 @@ class DiceGame
 {
 private:
     Player *player;
+    Human human;
+    System sys;
     Die *die;
 
 public:
+    // make sure player is not pointing to any memory location.
+    // It points to nothing.
+    DiceGame() : player(nullptr), die(nullptr)
+    {
+    }
     void init()
     {
+        /**
+         * 1. Get player name.
+         * 2. Ask player to roll a die.
+         * 3. Add the points to player's points
+         *
+         * Part 2:
+         * 1. Ask player to roll a die.
+         * 2. Ask system to roll a die too.
+         * 3. Add player's points to the one who won.
+         */
+
         // default set player as human
-        player = Human("Lionel");
+        // player = new Human("Lionel");
+        // sys = new System();
+        player = new Human("Lionel");
+
+        die = new Die();
+
+        bool quit = false;
+        int choice = 0, turn = 0, i = 0;
+        while (!quit)
+        {
+            turn++;
+            cout << "Player " << i % 2 + 1 << " roll a dice. \nRolling a die.." << endl;
+            die->roll();
+            const int faceValue = die->getFaceValue();
+            die->roll();
+            const int faceValue2 = die->getFaceValue();
+            die->roll();
+            const int faceValue3 = die->getFaceValue();
+            player->addPoints(faceValue);
+            player->setPoints(player->getName(), turn, (faceValue + faceValue2 + faceValue3));
+            cout << "Rolled a die with value: " << faceValue << " " << faceValue2 << " " << faceValue3 << endl;
+            cout << "Roll again? 0. NO, 1. YES: ";
+            cin >> choice;
+            if (choice == 0)
+            {
+                quit = true;
+                // cout << "Points" << player->getPoints() << endl;
+                displayStats(player);
+            }
+            i++;
+        }
     }
-    void displayStats() {}
+    void displayStats(Player *player)
+    {
+        for (auto p : player->getPlayers())
+        {
+            cout << "Turn: " << p.turn << "Name: " << p.name << " Points: " << p.points << endl;
+        }
+    }
+
+    ~DiceGame()
+    {
+        delete player;
+        delete die;
+        // delete human;
+        // delete sys;
+    }
 };
 
 int main()
 {
-    Player player;
-    cout << "Before placing a bet" << endl;
-    cout << player.getPoints() << endl;
-    player.bet(58);
-    cout << "After placing a bet." << endl;
-    cout << player.getPoints() << endl;
+    srand((unsigned)time(NULL));
+    DiceGame game;
+    game.init();
+
     return 0;
 }
